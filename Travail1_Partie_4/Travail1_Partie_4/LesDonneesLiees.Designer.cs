1bfce636d282e348779c47ffd70625086738acf0
JeffloveJeanfrancois <157496861+JeffloveJeanfrancois@users.noreply.github.com>
2025-04-08 17:38:48 -0400

    fore 3

--- Travail1_Partie_4/Travail1_Partie_4/LesDonneesLiees.Designer.cs
+++ Travail1_Partie_4/Travail1_Partie_4/LesDonneesLiees.Designer.cs
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LesDonneesLiees";
+            groupBox4 = new GroupBox();
+            dataGridView2 = new DataGridView();
+            groupBox3 = new GroupBox();
+            dataGridView5 = new DataGridView();
+            groupBox2 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            dataGridView3 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button_Recherchee = new Button();
+            label5 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button_Rechercher = new Button();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView5).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(dataGridView2);
+            groupBox4.Location = new Point(526, 287);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(468, 188);
+            groupBox4.TabIndex = 11;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Recherche de compagnie";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(31, 55);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(399, 100);
+            dataGridView2.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button_Rechercher);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(label2);
+            groupBox3.Controls.Add(textBox2);
+            groupBox3.Controls.Add(dataGridView5);
+            groupBox3.Location = new Point(526, 24);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(468, 219);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Recherche de compagnie";
+            // 
+            // dataGridView5
+            // 
+            dataGridView5.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView5.Location = new Point(17, 92);
+            dataGridView5.Name = "dataGridView5";
+            dataGridView5.Size = new Size(445, 93);
+            dataGridView5.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(30, 287);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(462, 188);
+            groupBox2.TabIndex = 10;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Recherche de compagnie";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 55);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(436, 100);
+            dataGridView1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(button_Recherchee);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(dataGridView3);
+            groupBox1.Location = new Point(30, 24);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(462, 219);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Recherche de compagnie";
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(6, 92);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(450, 93);
+            dataGridView3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 15);
+            label1.TabIndex = 4;
+            label1.Text = "selctionner la piece desiree";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(220, 34);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(96, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(231, 34);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(70, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // button_Recherchee
+            // 
+            button_Recherchee.Location = new Point(331, 22);
+            button_Recherchee.Name = "button_Recherchee";
+            button_Recherchee.Size = new Size(111, 45);
+            button_Recherchee.TabIndex = 6;
+            button_Recherchee.Text = "Recherche eagle lodding";
+            button_Recherchee.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 36);
+            label5.Name = "label5";
+            label5.Size = new Size(208, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Compagnie ou la piece de compagnie";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(148, 15);
+            label2.TabIndex = 8;
+            label2.Text = "selctionner la piece desiree";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 33);
+            label3.Name = "label3";
+            label3.Size = new Size(208, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Compagnie ou la piece de compagnie";
+            // 
+            // button_Rechercher
+            // 
+            button_Rechercher.Location = new Point(316, 22);
+            button_Rechercher.Name = "button_Rechercher";
+            button_Rechercher.Size = new Size(114, 46);
+            button_Rechercher.TabIndex = 10;
+            button_Rechercher.Text = "Recherche eagle lodding";
+            button_Rechercher.UseVisualStyleBackColor = true;
+            // 
+            // LesDonneesLiees
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1006, 513);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "LesDonneesLiees";
+            Text = "LesDonneesLiees";
+            groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView5).EndInit();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox4;
+        private DataGridView dataGridView2;
+        private GroupBox groupBox3;
+        private DataGridView dataGridView5;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView3;
+        private TextBox textBox2;
+        private Label label5;
+        private Button button_Recherchee;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Button button_Rechercher;
     }
 }
