4c85f02c00c899d9f40de3caeba8ea088381d079
JeffloveJeanfrancois <157496861+JeffloveJeanfrancois@users.noreply.github.com>
2025-04-14 10:19:27 -0400

    fome 3 accessible pas encore separer le code

--- Travail1_Partie_4/Travail1_Partie_4/LesDonneesLiees.cs
+++ Travail1_Partie_4/Travail1_Partie_4/LesDonneesLiees.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.EntityFrameworkCore;
+using Travail1_Partie_4.Models;
 
 namespace Travail1_Partie_4
 {
     public partial class LesDonneesLiees : Form
     {
+        List<TblCompagnie> compagnies;
+        Bd_ReseauContext context1;
         public LesDonneesLiees()
         {
             InitializeComponent();
+        }
+        private void DesactiverLesColonnesenTrop(DataGridView dataGridView)
+        {
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            // dataGridView.Columns["Compagnies"].Visible = false;
+        }
+
+        private void button_Recherchee_Click(object sender, EventArgs e)
+        {
+            using (var context = new Bd_ReseauContext())
+            {
+                compagnies = context.TblCompagnies
+                    .Include(c => c.TblProjets)
+                    .ToList();
+
+                eagleLoading_DataGridView.DataSource = compagnies;
+
+                DesactiverLesColonnesenTrop(eagleLoadingCompagnie_DataGridView);
+                eagleLoading_DataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                eagleLoading_DataGridView.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+                //PrendreLaColonneChoisir(e);
+
+            }
+        }
+
+
+        private void LesDonneesLiees_Load(object sender, EventArgs e)
+        {
+
+        }
+       private int PrendreLaPieceChoisir(DataGridView dataGridView)
+        {
+            return (int)dataGridView[0, dataGridView.CurrentRow.Index].Value;
+        }
+
+        private void eagleLoading_DataGridView_Click(object sender, EventArgs e)
+        {
+            int noCompagnie = PrendreLaPieceChoisir(eagleLoading_DataGridView);
+            var pieceRechercher = compagnies.FirstOrDefault(p=> p.IdCompagnie == noCompagnie);
+            eagleLoadingCompagnie_DataGridView.DataSource = pieceRechercher.TblProjets;
+
         }
     }
 }
